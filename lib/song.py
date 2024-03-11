8f653c8d39ca697d95f84ca084bf8191b3aedfeb
Gilbert <shikweg@gmail.com>
2024-03-11 10:32:45 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,39 @@
 class Song:
-    pass
+    genre_count = {}
+    artist_count = {}  # Added artist_count dictionary
+    count = 0
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else:
+            Song.artist_count[self.artist] = 1
+
+# Example usage:
+song1 = Song("99 Problems", "Jay Z", "Rap")
+song2 = Song("Halo", "Beyonce", "Pop")
+song3 = Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+song4 = Song("Sara Smile", "Hall and Oates", "Pop")
+
+print("Genre Count:", Song.genre_count)
+print("Artist Count:", Song.artist_count)
+print("Total Count:", Song.count)
+
